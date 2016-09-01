5bf2c2322d84f2a998b3d7b03799352a06ea4d42
Kamlesh <kamlesh.c89@gmail.com>
2016-09-01 17:04:00 +0530

    Add files via upload

---
+++ ldapsearch.py
@@ -0,0 +1,6 @@
+ import ldap
+l = ldap.initialize('ldap://localhost:389')
+#l.search_s('dc=example,dc=com',ldap.SCOPE_SUBTREE)
+r = l.search_s('dc=example,dc=com',ldap.SCOPE_SUBTREE,)
+for dn,entry in r:
+   print 'Processing',repr(dn)
